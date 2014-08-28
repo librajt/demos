2838ea57860ef5e9aa2ad7b4dfb16b20d7f7b760
librajt <librajt@live.cn>
2014-08-28 23:58:22 +0800

    1

---
+++ package/17-ng-form-generator/js/dirlist.js
@@ -0,0 +1,18 @@
+window.dirList = [
+    {  
+        'first_dir':'F1',
+        'second_dir':['F1_1','F1_2','F1_3'],
+        'hot_dir':{
+            '1-500热吧' : 'adsense_hotdir_b0dc2f50672971ce20ffc144c520476f_1',
+            '500-2000热吧' : 'adsense_hotdir_b0dc2f50672971ce20ffc144c520476f_2'
+        }
+    },
+    {  
+        'first_dir':'F2',
+        'second_dir':['F2_1','F2_2','F2_3'],
+        'hot_dir':{
+            '1-500热吧' : 'adsense_hotdir_b0dc2f50672971ce20ffc144c520476f_3',
+            '500-2000热吧' : 'adsense_hotdir_b0dc2f50672971ce20ffc144c520476f_4'
+        }
+    }
+];
---
+++ package/17-ng-form-generator/js/relation.js
@@ -0,0 +1,133 @@
+window.RULES_TYPES={
+    "PC": {
+        "FRS": [{
+            "pos_name": "3",
+            "pos_desc": "3楼",
+            "rules": ["0001", "0002", "0003", "0004", "0010", "0008"]
+        },
+        {
+            "pos_name": "15",
+            "pos_desc": "15楼",
+            "rules": ["0001", "0002", "0003", "0004"]
+        },
+        {
+            "pos_name": "25",
+            "pos_desc": "25楼",
+            "rules": ["0001", "0002", "0003", "0004"]
+        },
+        {
+            "pos_name": "THREAD_TOP",
+            "pos_desc": "贴子列表上方",
+            "rules": ["0005"]
+        },
+        {
+            "pos_name": "POSTER_TOP",
+            "pos_desc": "发贴框上方",
+            "rules": ["0006"]
+        },
+        {
+            "pos_name": "NAV_TOP",
+            "pos_desc": "导航条顶部",
+            "rules": ["0009"]
+        },
+        {
+            "pos_name": "7",
+            "pos_desc": "7楼",
+            "rules": ["0011"]
+        }],
+        "PB": [{
+            "pos_name": "2",
+            "pos_desc": "2楼",
+            "rules": ["0105", "0106", "0107"]
+        },
+        {
+            "pos_name": "3",
+            "pos_desc": "3楼",
+            "rules": ["0101", "0102", "0103"]
+        },
+        {
+            "pos_name": "15",
+            "pos_desc": "15楼",
+            "rules": ["0101", "0102", "0103"]
+        },
+        {
+            "pos_name": "25",
+            "pos_desc": "25楼",
+            "rules": ["0101", "0102", "0103"]
+        }]
+    },
+    "WAP": {
+        "FRS": [{
+            "pos_name": "3",
+            "pos_desc": "3楼",
+            "rules": ["1001"]
+        },
+        {
+            "pos_name": "15",
+            "pos_desc": "15楼",
+            "rules": ["1001"]
+        },
+        {
+            "pos_name": "25",
+            "pos_desc": "25楼",
+            "rules": ["1001"]
+        },
+        {
+            "pos_name": "5",
+            "pos_desc": "5楼",
+            "rules": ["1001"]
+        },
+        {
+            "pos_name": "7",
+            "pos_desc": "7楼",
+            "rules": ["1001"]
+        }],
+        "PB": [{
+            "pos_name": "3",
+            "pos_desc": "3楼",
+            "rules": ["1101"]
+        },
+        {
+            "pos_name": "15",
+            "pos_desc": "15楼",
+            "rules": ["1101"]
+        },
+        {
+            "pos_name": "25",
+            "pos_desc": "25楼",
+            "rules": ["1101"]
+        }]
+    },
+    "APP": {
+        "FRS": [{
+            "pos_name": "3",
+            "pos_desc": "3楼",
+            "rules": ["2001"]
+        },
+        {
+            "pos_name": "15",
+            "pos_desc": "15楼",
+            "rules": ["2001"]
+        },
+        {
+            "pos_name": "25",
+            "pos_desc": "25楼",
+            "rules": ["2001"]
+        }],
+        "PB": [{
+            "pos_name": "3",
+            "pos_desc": "3楼",
+            "rules": ["2101"]
+        },
+        {
+            "pos_name": "15",
+            "pos_desc": "15楼",
+            "rules": ["2101"]
+        },
+        {
+            "pos_name": "25",
+            "pos_desc": "25楼",
+            "rules": ["2101"]
+        }]
+    }
+}
