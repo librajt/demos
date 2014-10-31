1bb439ecf6d61080357b3224f335bfdaf59d6862
unknown <rentao@B95E2D85056411B.internal.baidu.com>
2014-10-31 18:47:44 +0800

    1

--- package/17-ng-form-generator/j_template.js
+++ package/17-ng-form-generator/j_template.js
@@ -27,6 +27,7 @@ window.json = [
         "id": "thread_pic1",
         "name": "贴子图片1",
         "type": "upload",
+        "hide": true,
         "filetype": "image",
         "width": 120,
         "height": 90,
@@ -41,13 +42,9 @@ window.json = [
         "height": 90,
         "whitelist": "png|jpg|jpeg"
     },
-    {
-        "id": "thread_pic3",
-        "name": "贴子图片3",
-        "type": "upload",
-        "filetype": "image",
-        "width": 120,
-        "height": 90,
-        "whitelist": "png|jpg|jpeg"
-    }
+{"id":"rank_level","name":"星级","type":"select","options":[1,2,3,4,5,6,7,8,9,10]},
+{"id":"button_text","name":"按钮文案","type":"simpletext","validate":{"require":0,"doubleLength":4}},
+{"id":"label_visible","name":"标签显示","type":"select","options":[{"key":"是","val":1},{"key":"否","val":0}]},
+{"id":"thread_type","name":"星级文案","type":"simpletext","defaultValue":"贴吧推荐","validate":{"doubleLength":4}}
+
 ];
