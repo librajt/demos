79a94e9a0169d47b63ed5c3017afc4c4565effca
unknown <rentao@B95E2D85056411B.internal.baidu.com>
2015-05-08 19:22:02 +0800

    1

--- package/21-js-image-zoom-viewer/directive/ibe-setup-panel/ibe-setup-panel.js
+++ package/21-js-image-zoom-viewer/directive/ibe-setup-panel/ibe-setup-panel.js
@@ -39,38 +39,61 @@ angular.module('ngCommon').directive('ibeSetupPanel', [ function() {
             }
         ];
 
-        $(document).on('keydown', function(e) {
+        $(document).on('keydown', keyDownEvent);
+        $(document).on('keyup', keyUpEvent);
+
+        var keyMap = {
+            17: 'Control',
+            37: 'Left',
+            38: 'Up',
+            39: 'Right',
+            40: 'Down'
+        };
+        var isControlPress = false;
+
+        function keyDownEvent(e) {
             if (!scope.ngModel || !scope.helper || !scope.helper.visible) {
                 return;
             }
 
             var keyCode = e.keyCode;
-            var map = {
-                17: 'Control',
-                37: 'Left',
-                38: 'Up',
-                39: 'Right',
-                40: 'Down'
-            };
-
             switch (keyCode) {
-                case 37:
-                    scope.ngModel.position.left--;
-                    break;
-                case 39:
-                    scope.ngModel.position.left++;
-                    break;
-                case 38:
-                    scope.ngModel.position.top--;
-                    break;
-                case 40:
-                    scope.ngModel.position.top++;
+                case 17:
+                    isControlPress = true;
                     break;
             }
 
             scope.$apply();
             e.preventDefault();
-        });
+        }
+
+        function keyUpEvent(e) {
+            if (!scope.ngModel || !scope.helper || !scope.helper.visible) {
+                return;
+            }
+
+            var keyCode = e.keyCode;
+            switch (keyCode) {
+                case 17:
+                    isControlPress = false;
+                    break;
+                case 37:
+                    isControlPress ? scope.ngModel.size.width-- : scope.ngModel.position.left--;
+                    break;
+                case 39:
+                    isControlPress ? scope.ngModel.size.width++ : scope.ngModel.position.left++;
+                    break;
+                case 38:
+                    isControlPress ? scope.ngModel.size.height-- : scope.ngModel.position.top--;
+                    break;
+                case 40:
+                    isControlPress ? scope.ngModel.size.height++ : scope.ngModel.position.top++;
+                    break;
+            }
+
+            scope.$apply();
+            e.preventDefault();
+        }
 
         scope.$watch('ngModel', function() {
             ;
